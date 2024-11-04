f4bc48f480887984fac71d2892ebd74d3bf70f39
subtubes-io <edgar@subtubes.io>
2024-11-03 17:05:06 -0800

    Code cleanup

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# curl -X POST \
-#     "http://localhost:8888/apis/ccompat/v6/subjects/MessageEvent/versions" \
-#     -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-#     -d '{"schema": "{\"type\": \"record\", \"name\": \"MessageEvent\", \"namespace\": \"com.example.events\", \"fields\": [{\"name\": \"text\", \"type\": \"string\"}, {\"name\": \"timestamp\", \"type\": \"string\"}]}", "schemaType": "AVRO"}'
-
 function register {
     if curl -X POST http://localhost:8081/subjects/MessageEventJson/versions \
         -H "Content-Type: application/vnd.schemaregistry.v1+json" \
